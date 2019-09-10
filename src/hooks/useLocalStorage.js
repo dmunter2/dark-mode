723cf792a3c54c24a727844ce0d293bc62b2b65a
dmunter2 <devon.munter@gmail.com>
2019-09-09 20:48:08 -0700

    local-storage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+    // const item = JSON.parse(window.localStorage.getItem(key));
+
+    const [storedValue, setStoredValue] = useState(() => {
+        // Get from local storage by key
+        const item = window.localStorage.getItem(key);
+        // Parse and return stored json or, if undefined, return initialValue
+        return item ? JSON.parse(item) : initialValue;
+    });
+    return [storedValue];
+
+}
